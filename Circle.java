0d11ca5bce727ab6eedc06d7fcb3faef7603c2fa
Susanta Maity <119559211+Susanta252@users.noreply.github.com>
2022-12-01 13:50:37 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+public class Circle
+{
+public static void main(String args[])
+{
+int a =80;
+float b = 5;
+float area_circle = a*b;
+System.out.println("Area of Circle is "+area_circle);
+}
+}
---
+++ Rect.java
@@ -0,0 +1,10 @@
+public class Rect
+{
+public static void main(String args[])
+{
+int a =80;
+int b =30;
+int Perimeter_Rect= 2*(a+b);
+System.out.println("Perimeter of Rect is"+Perimeter_Rect);
+}
+}
